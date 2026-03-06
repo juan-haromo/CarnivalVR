6729b1bc69499cebccab7d54aa113486261a6d80
Juan Haro <juan.haro.dev@gmail.com>
2026-03-06 13:47:48 -0600

    Fix look around tutorial

--- Assets/Scripts/Tutorial/LookAroundTutorial.cs
+++ Assets/Scripts/Tutorial/LookAroundTutorial.cs
@@ -5,7 +5,6 @@ public class LookAroundTutorial : MonoBehaviour
 {
     [SerializeField] InputActionReference lookReference;
     [SerializeField] int times;
-    float nexTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,9 +14,8 @@ public class LookAroundTutorial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (nexTime <= Time.deltaTime && lookReference.action.ReadValue<Vector2>().x != 0 )
+        if(lookReference.action.WasPressedThisFrame() && lookReference.action.ReadValue<Vector2>().x != 0)
         {
-            nexTime++;
             times--;
             if (times <= 0)
             {
