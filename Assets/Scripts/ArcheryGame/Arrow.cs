3c22ea271b185909f2ea39c793694f13fd14720d
Juan Haro <juan.haro.dev@gmail.com>
2026-03-24 21:16:08 -0600

    Fix arrow trajectory
    Now arrow is looking towards its movement makint the trakectory look and feel better

--- Assets/Scripts/ArcheryGame/Arrow.cs
+++ Assets/Scripts/ArcheryGame/Arrow.cs
@@ -4,9 +4,17 @@ public class Arrow : MonoBehaviour
 {
     [SerializeField] float lifeTime;
     [SerializeField] Rigidbody rb;
+    [SerializeField] Transform arrowMehs;
+    bool isUpdating = true;
 
+    void Update()
+    {
+        if(!isUpdating){return;}
+        arrowMehs.forward = rb.linearVelocity.normalized;   
+    }
     void OnTriggerEnter(Collider other)
     {
+        isUpdating = false;
         Destroy(gameObject,lifeTime);
         rb.isKinematic = true;
         rb.detectCollisions =false;
