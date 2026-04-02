52c1c722c1fe266acad860e14022d17bdb6857cf
Juan Haro <juan.haro.dev@gmail.com>
2026-04-02 11:46:15 -0600

    Add pool structure for game objects

---
+++ Assets/Scripts/Pool/GameObjectPool.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectPool : MonoBehaviour
+{
+    [SerializeField] private int initialPoolSize = 3;
+    [SerializeField] private PooledObject interactablePooledObject;
+    private Stack<PooledObject> stack;
+
+    void Start()
+    {
+        SetUpPool();
+    }
+
+    private void SetUpPool()
+    {
+        stack = new Stack<PooledObject>(initialPoolSize);
+        PooledObject instance;
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            instance = CreateNewPooledObject();
+            stack.Push(instance);
+        }
+    }
+
+    private PooledObject CreateNewPooledObject()
+    {
+        PooledObject instance = Instantiate(interactablePooledObject).GetComponent<PooledObject>();
+        instance.gameObject.SetActive(false);
+        instance.Pool = this;
+        return instance;
+    }
+
+    public PooledObject GetPooledObject()
+    {
+        if (stack.Count > 0)
+        {
+            return stack.Pop();
+        }
+        else
+        {
+            return CreateNewPooledObject();
+        }
+    }
+
+    public void ReturnToPool(PooledObject pooledObject)
+    {
+        stack.Push(pooledObject);
+        pooledObject.gameObject.SetActive(false);
+    }
+}
